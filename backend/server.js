0434811da70a9c1dc71121feb3bb05c6cc50d1c1
Vivek Patel <142316463+Patelviveke@users.noreply.github.com>
2026-06-15 18:49:41 +0530

    fixed CORS and socket connection

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,11 @@ const path = require("path");
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
-  cors: { origin: "*" }
+  cors: { 
+    origin: "*" ,
+    methods: ["GET", "POST"],
+    credentials: true
+  }
 });
 
 app.use(express.static(path.join(__dirname, "../frontend")));
